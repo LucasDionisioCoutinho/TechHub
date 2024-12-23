4c70ee6a05c1b8c2c0506ce39c06b4723b30db4c
Lucas Dionísio Coutinho <lukasdionisio00@gmail.com>
2024-12-22 22:06:44 -0300

    Att no site TechHub

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
 
 
-    
+document.getElementById("explorar").addEventListener("click", function(){
+    const produtosDestaques = document.getElementById("produtos-destaque");
+    if (produtosDestaques) {
+        produtosDestaques.scrollIntoView({ behavior: "smooth", block: "start"});
+    }
+})
