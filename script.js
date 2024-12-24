66fa56dec6775f5baea664608603ff04a54ca8b8
Lucas Dionísio Coutinho <lukasdionisio00@gmail.com>
2024-12-23 22:25:07 -0300

    Atualizaçao do site Tech

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 
 
-
+/*
 document.getElementById("explorar").addEventListener("click", function(){
     const produtosDestaques = document.getElementById("produtos-destaque");
     if (produtosDestaques) {
         produtosDestaques.scrollIntoView({ behavior: "smooth", block: "start"});
     }
 })
+*/
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nvaLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    nvaLinks.classList.toggle('active');
+});
